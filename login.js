acc967ec4b71bea311bca5122aa960718794c459
razedulislam <blindtech2021@gmail.com>
2022-11-10 09:14:49 +0000

    login form now working

--- login.js
+++ login.js
@@ -5,8 +5,8 @@ document.getElementById('submitBtn').addEventListener('click' , function(){
     const rePasswaordField = document.getElementById('rePasswaordField').value ;
     console.log(rePasswaordField) ;
     if(emailField == 'raj2022@gmail.com' && passwaordField == 'raj2022' && rePasswaordField =='raj2022'){
-        location.href = 'http://127.0.0.1:5500/bank%20transaction%20project/bankWithdrowDepo.html';
+        // location.href = 'http://127.0.0.1:5500/bank%20transaction%20project/bankWithdrowDepo.html';
         
-        // window.location.href ='bankWithdrowDepo.html' ;
+        window.location.href ='bankWithdrowDepo.html' ;
     }
 })
